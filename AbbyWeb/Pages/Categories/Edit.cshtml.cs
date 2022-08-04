05c5f5d9cc29e0651a6ea9f1f03374db9ccbe120
Ervin Tuzlic <ervin@fragnet.net>
2022-08-05 00:18:18 +0200

    Edit Category

---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        //** BIND PROPERTY IT AUTOMATICALLY BINDS WITH UI AND IT'S POPULATED IN THE SAME OBJECT (CATEGORY)
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        //** Name can be used with OnPost ex. OnPostCreate()
+        public async Task<IActionResult> OnPost()
+        {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The DisplayOrder cannot be the same as name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
